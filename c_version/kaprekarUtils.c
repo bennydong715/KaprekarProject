46fbfc67a705465a8e1eb90b95a270f7b566116d
bennydong715 <54565554+bennydong715@users.noreply.github.com>
2019-09-13 20:02:22 -0500

    Update kaprekarUtils.c

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -37,18 +37,20 @@ int isKaprekar(int n) {
   long first, second;
 
   //for each possible "split" of the square...
-  for(i=1; i<=numberOfDigits; i++) {
+count = 0;
+  while(i != 0){
     //increase the modulus by a factor of 10
-    modulous *= 10;
-
+    i /= 10;
+    count ++;
+}
     //split the square into two parts
     first = square / modulus;
     second = square % modulus;
-
+    sum = first + second;
     //test if the split makes a Kaprekar number
     //the second part cannot be zero and if
     //the sum of the two parts is equal to n, then it is
-    if(second > 0 && n == first + second) {
+    if(second != 0 && n == sum) {
       return 1;
     }
   }
